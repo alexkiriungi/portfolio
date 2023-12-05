1221001f701cfb51cf955af48aa699189d0778c4
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2023-12-05 05:21:28 +0300

    Completed the Skeleton phase, Bug fixing tomorrow

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 const header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
 
 window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.scrollY > 120);
 });
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navlist.classList.toggle("active")
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove("active");
+};
