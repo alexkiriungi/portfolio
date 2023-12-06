4cd49bd1512f6f52129eb1aa5228fe1b3465f93a
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2023-12-06 21:56:14 +0300

    Added textanimation and proper links

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const header = document.querySelector("header");
 let menu = document.querySelector("#menu-icon");
 let navlist = document.querySelector(".navlist");
+const btn = document.querySelector(".send-btn");
+const btn1 = document.querySelector(".top-btn");
 
 window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.scrollY > 120);
@@ -15,3 +17,11 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove("active");
 };
+
+btn.addEventListener('click', ()=> {
+    alert("Please Email me at: mwasxander@gmail.com")
+});
+
+btn1.addEventListener('click', ()=> {
+    alert("Please Email me at: mwasxander@gmail.com")
+});
